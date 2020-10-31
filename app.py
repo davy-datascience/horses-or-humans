631ad2a37953d0aef54f8458684757d86fed170c
davy-datascience <davy.datascience@gmail.com>
2020-11-01 00:49:48 +0100

    Upload on aws + prediction OK

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import json
 import os
 import boto3
 from botocore.config import Config
+from botocore.exceptions import ClientError
 from flask import Flask, render_template, request, redirect, url_for, session
-
 import numpy as np
 from keras.preprocessing import image
 from keras.models import load_model
 from classes.prediction import Prediction, get_predictions_json
+import logging
+import uuid
 
 app = Flask(__name__)
 app.secret_key = b'c\x83\xc5MU\x9eV\xd2R\x12\x87(\x8c\xb6S\xf3H\xbc\xedn\xa3\xa3\tl'
@@ -26,27 +28,40 @@ def predict():
     return render_template('predict.html', predictions=predictions, title="Predictions")
 
 
+def upload_file(file_name, bucket):
+    """
+    Function to upload a file to an S3 bucket
+    """
+    object_name = file_name
+    s3_client = boto3.client('s3')
+    response = s3_client.upload_file(file_name, bucket, object_name)
+
+    return response
+
+
 @app.route('/', methods=['POST'])
-def upload_file():
+def upload():
+    bucket = "horses-or-humans"
     model = load_model('model.h5')
     predictions = []
-    for uploaded_file in request.files.getlist('file'):
-        if uploaded_file.filename != '':
-            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename))
-            img = image.load_img("static/uploads/" + uploaded_file.filename, target_size=(300, 300))
-            x = image.img_to_array(img)
-            x = np.expand_dims(x, axis=0)
+    for f in request.files.getlist('file'):
+        f.save(os.path.join('static/uploads', f.filename))
+        upload_file(f"static/uploads/{f.filename}", "horses-or-humans")
 
-            images = np.vstack([x])
-            classes = model.predict(images, batch_size=10)
-            prediction = None
-            if classes[0] > 0.5:
-                print(uploaded_file.filename + " is a human")
-                prediction = Prediction(uploaded_file.filename, "human")
-            else:
-                print(uploaded_file.filename + " is a horse")
-                prediction = Prediction(uploaded_file.filename, "horse")
-            predictions.append(prediction)
+        img = image.load_img(f"static/uploads/{f.filename}", target_size=(300, 300))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+
+        images = np.vstack([x])
+        classes = model.predict(images, batch_size=10)
+        prediction = None
+        if classes[0] > 0.5:
+            print(f.filename + " is a human")
+            prediction = Prediction(f.filename, "human")
+        else:
+            print(f.filename + " is a horse")
+            prediction = Prediction(f.filename, "horse")
+        predictions.append(prediction)
     predictions_json = get_predictions_json(predictions)
     predictions_str = json.dumps(predictions_json)
     session['predictions'] = predictions_str
